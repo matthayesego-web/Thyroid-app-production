78e0a29be1d5bd3b849fe447f29e3bdf51ea2404
matthayesego-web <matt.hayesego@gmail.com>
2026-08-26 22:17:44 -0400

    v0.3.4: bump internal test version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.thyroidtracker.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.3"
+        versionCode = 7
+        versionName = "0.3.4"
     }
 
     buildFeatures {
